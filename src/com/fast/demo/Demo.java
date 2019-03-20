7543a8188b1d2069eefc6e067ec5ff4705e77272
shinstein <shinstein@126.com>
2019-03-20 22:14:20 +0800

    更新代码注释

--- src/com/fast/demo/Demo.java
+++ src/com/fast/demo/Demo.java
@@ -1,5 +1,8 @@
 package com.fast.demo;
 
+/**
+ * 这是传到github的第一个项目
+ */
 public class Demo {
     public static void main(String[] args) {
         System.out.printf("this is my first github program.");
